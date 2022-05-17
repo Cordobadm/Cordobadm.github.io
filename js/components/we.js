e51625da37e0149f0a953eac83be688ef1b5d8c9
Diego Cordoba <cordobadiegom@gmail.com>
2022-05-17 13:01:18 -0300

    cambios en nosotras

--- js/components/we.js
+++ js/components/we.js
@@ -1,3 +1,5 @@
+// Corregir tapa el nombre de alexia, tamaño de los cuadros
+
 const obtenerPersonas = () => {
   const URLGET = "../js/costants/we.json";
 
@@ -7,34 +9,15 @@ const obtenerPersonas = () => {
       <div id="cardNos" class="mt-3 col-12 col-md-6 col-lg-4">
       <div class="card">
         <div class="card-profile">
-          <div
-            id="carouselExampleSlidesOnly"
-            class="carousel slide"
-            data-bs-pause="false"
-            data-bs-ride="carousel"
-            data-bs-interval="3000"
-          >
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="${card.imagen}" alt="imagen1" />
-              </div>
-              <div class="carousel-item">
-                <img src="${card.imagen1}" alt="imagen2" />
-              </div>
-              <div class="carousel-item">
-                <img src="${card.imagen2}" alt="imagen3" />
-              </div>
-            </div>
-          </div>
+        <img
+        src="${card.imagen}"
+        class="card-img-top"
+        alt="imagenDra"
+        />
         </div>
     
         <div class="card-body">
-          <div class="badges">
-            <span class="badge rounded-pill bg-warning text-dark"
-              >${card.cargo}</span
-            >
-            <span class="badge rounded-pill bg-primary">${card.profesion}</span>
-          </div>
+         
           <h5 id="name" class="card-title p-2">${card.nombre} ${card.apellido}</h5>
     
           <p id="info" class="card-text">${card.informacion}</p>
@@ -47,14 +30,8 @@ const obtenerPersonas = () => {
             <a href="${card.linkedin}" target="_blank">
               <img src="../img/iconos/iconoLinkedin.svg" alt="twitter"
             /></a>
-            <a href="${card.instagram}" target="_blank"
-              ><img src="../img/iconos/iconoig.svg" alt="instagram"
-            /></a>
-            <a href="${card.facebook}" target="_blank">
-              <img src="../img/iconos/iconoFace.svg" alt="facebook"
-            /></a>
-            <a href="${card.twitter}" target="_blank">
-              <img src="../img/iconos/iconoTwitter.svg" alt="twitter"
+            <a href="${card.instagram}" target="_blank">
+              <img src="../img/iconos/iconoig.svg" alt="twitter"
             /></a>
           </div>
         </div>
