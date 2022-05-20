d42679846c24cb0cde28f11a2801ce18b7eda6d3
Diego Cordoba <cordobadiegom@gmail.com>
2022-05-20 12:00:15 -0300

    add nuevo filtro de busqueda

--- js/components/allProducts.js
+++ js/components/allProducts.js
@@ -11,35 +11,32 @@ function addProducts(arrayProd) {
 
   arrayProd.map((e) => {
     cardProducts.innerHTML += `
-      <div id="cardService" class= "${e.rubro} col-6 col-md-4 col-lg-3"> 
-      <div class="item-rounded">
-      <div class="image">
-        <img src="${e.img}" >
-      </div>
-      <h6 class="card-title">${e.marca}</h6>
+      <div id="cardService" class= "${e.rubro} ${e.subRubro} col-6 col-md-4 col-lg-3"> 
+      <div class="  item-rounded">
+        <a target="_blank" href="${e.img}">
+          <img src="${e.img}" id="myImg">
+        </a>
         <h6 class="card-title">${e.nombre}</h6>
         <h4>Precio: $<span>${e.precio}</span></h4>
         <h3>stock: ${e.stock}</h3>
     
         <li id="turnos">
-          <a href="https://api.whatsapp.com/send/?phone=543813846221&text=¡Hola Chicas! ¿Tienen stock de  ${e.nombre}?" target="_blank">
+          <a href="https://api.whatsapp.com/send/?phone=543813846221&text=¡Hola Chicas! Necesito turno para ${e.nombre}" target="_blank">
           <img src="../img/iconos/iconoWhatsapp.png" alt="imagen whatsapp">Consultar Stock</a>
         </li>
         </div>
       </div>
     `;
-    
   });
-  
 }
 
-// AGREGAR FILTRADO POR SUBRUBRO
+// FILTRAR PRODUCTOS POR RUBRO
 
-let productos = document.getElementById("products");
+let productos = document.getElementById("produtos");
 const filtrarProductos = () => {
   if (productos.value === "all") {
     addProducts(stockProducts);
-  } else  {
+  } else {
     const arrayFiltrado = stockProducts.filter(
       (el) => el.rubro == productos.value
     );
@@ -49,18 +46,85 @@ const filtrarProductos = () => {
 
 productos.addEventListener("change", function () {
   filtrarProductos();
+  subRubro()
 });
 
-const filtrarPrecios = document.getElementById("prices");
+//FILTRAR PRODUCTOS POR SUBRUBRO (makeUp)
+let makeup = document.getElementById("makeup");
+const filtrarSubRubro = () => {
+  if (makeup.value === "all") {
+    const arrayFiltrado = stockProducts.filter(
+      (el) => el.rubro == productos.value
+    );
+    addProducts(arrayFiltrado);
+  } else {
+    const arrayFiltrado1 = stockProducts.filter(
+      (el) => el.subRubro == makeup.value
+    );
+    addProducts(arrayFiltrado1);
+  }
+};
+
+makeup.addEventListener("change", function () {
+  filtrarSubRubro();
+});
+
+//FILTRAR PRODUCTOS POR SUBRUBRO (cuidadosDeLaPiel)
+let cuidadosDeLaPiel = document.getElementById("cuidadosDeLaPiel");
+const filtrarSubRubroCuidadosPiel = () => {
+  if (cuidadosDeLaPiel.value === "all") {
+    const arrayFiltrado = stockProducts.filter(
+      (el) => el.rubro == productos.value
+    );
+    addProducts(arrayFiltrado);
+  } else {
+    const arrayFiltrado1 = stockProducts.filter(
+      (el) => el.subRubro == cuidadosDeLaPiel.value
+    );
+    addProducts(arrayFiltrado1);
+  }
+};
+
+cuidadosDeLaPiel.addEventListener("change", function () {
+  filtrarSubRubroCuidadosPiel();
+});
+
+//FILTRAR PRODUCTOS POR PRECIO
+
+const filtrarPrecios = document.getElementById("precio");
+
 filtrarPrecios.addEventListener("change", () => {
   if (filtrarPrecios.value == 1) {
     stockProducts.sort((a, b) => a.precio - b.precio);
     filtrarProductos(stockProducts);
+    
   } else {
     stockProducts.sort((a, b) => b.precio - a.precio);
     filtrarProductos(stockProducts);
+    
   }
 });
 
-
+const subRubro = () => {
+  if (productos.value === "all") {
+    $(makeup).hide();
+    $(cuidadosDeLaPiel).hide();
+    $(peinado).hide();
+  }
+  if (productos.value == "MAKEUP") {
+    $(makeup).show();
+    $(cuidadosDeLaPiel).hide();
+    $(peinado).hide();
+  }
+  if (productos.value == "PEINADO") {
+    $(makeup).hide();
+    $(cuidadosDeLaPiel).hide();
+    $(peinado).show();
+  }
+  if (productos.value == "CUIDADOSPIEL") {
+    $(makeup).hide();
+    $(cuidadosDeLaPiel).show();
+    $(peinado).hide();
+  }
+};
 
